c77a5666bd0d182ade2fa614966ac1b4518d6249
olegopro <evilgazz@gmail.com>
2021-05-03 08:05:56 +0300

    Код урока Open-Closed Principle

---
+++ src/Classes/O/DataBaseSave.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\O;
+
+
+use App\Saver;
+use mysqli;
+
+class DataBaseSave implements Saver
+{
+
+    private $mysqli, $host, $user, $pass, $db;
+
+    public function __construct($host, $user, $pass, $db)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->db   = $db;
+    }
+
+    public function connect()
+    {
+        $this->mysqli = new mysqli($this->host, $this->user, $this->pass, $this->db);
+
+        if ($this->mysqli->connect_errno) {
+            echo "Failed to connect to MySQL: " . $this->mysqli->connect_error;
+            exit();
+        }
+    }
+
+    public function save($data)
+    {
+        $this->connect();
+        $this->mysqli->query("INSERT INTO `report` (report) VALUES ('" . $data . " ')");
+    }
+
+
+}
