3594c1327c6fb83713d409a4db4e0dc23cbf9893
Oleg Desyatnikov <evilgazz@yandex.ru>
2021-05-04 21:09:06 +0300

    почистил код

--- src/index.php
+++ src/index.php
@@ -1,15 +1,17 @@
 <?php
 
+
+require '../vendor/autoload.php';
+
+
 use App\O\DataBaseSave;
 use App\O\FileSave;
 use App\O\Report;
 use App\O\ReportRepository;
 
-require '../vendor/autoload.php';
-
 $report = new Report();
 $report->renderReport();
 
 $repository = new ReportRepository($report, new FileSave('file.txt'));
-$repository = new ReportRepository($report, new DataBaseSave('localhost', 'root', '', 'demo'));
+//$repository = new ReportRepository($report, new DataBaseSave('localhost', 'root', '', 'demo'));
 $repository->save();
